eef03e1c2bb677f030f5846e574665cd2bc6f893
maif712 <maif221@gmail.com>
2024-03-10 15:38:48 +0330

    fixed some hover bugs

--- src/components/GoTopBtn.jsx
+++ src/components/GoTopBtn.jsx
@@ -6,9 +6,9 @@ export default function GoTopBtn() {
     const { isShowBtn, handleScrollToTopClick } = useAppContext()
 
     return (
-        <div className={`fixed bottom-0 right-3 group translate-y-full transition-all betterhover:hover:!-translate-y-2 ${isShowBtn ? "!translate-y-0 !bottom-3" : ""}`}>
+        <div className={`go-top-btn fixed bottom-0 right-3 group translate-y-full transition-all ${isShowBtn ? "!translate-y-0 !bottom-3" : ""}`}>
             <button onClick={handleScrollToTopClick} className={`rounded-full p-3 shadow-md text-white bg-blue-400 ${isShowBtn ? "gelatine": ""}`}>
-                <div className="transition-all duration-300 betterhover:group-hover:-translate-y-1">
+                <div className="arrow-up-icon transition-all duration-300">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
                     </svg>
