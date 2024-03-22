aa7454b96123494f4e84432741eae16b30d9d457
maif712 <maif221@gmail.com>
2024-03-22 17:16:14 +0330

    close the more div on ousite click

--- src/components/Challenge.jsx
+++ src/components/Challenge.jsx
@@ -6,6 +6,7 @@ import ProgressBar from "./ProgressBar"
 import UpdateForm from "./UpdateForm"
 import useUpdateForm from "../hooks/useUpdateForm"
 import Mask from "./Mask"
+import { useEffect, useRef } from "react"
 
 
 export default function Challenge({index, id, title, challengeDays, handleOpenDeleteOneChallengeModal }) {
@@ -16,23 +17,55 @@ export default function Challenge({index, id, title, challengeDays, handleOpenDe
         handleSetIsExpandToFalse
     } = useExpand()
 
+    const moreDiv = useRef()
+    const dotRef = useRef()
+
     const {progressBarValueNow} = useProgressBar(challengeDays)
 
     const {isUpdateForm, handleSetIsUpdateForm, handleCloseUpdateForm} = useUpdateForm()
 
+    const hideExp = e => {
+        if(e.target != moreDiv.current)
+        {
+            handleSetIsExpandToFalse()
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleOutsideClick);
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick);
+        };
+    }, [])
+
+    const handleOutsideClick = (e) => {
+
+        if(e.target == dotRef.current)
+        {
+            handleIsExpand()
+            return;
+        }
+
+        // or we can write newRef.current !== e.target
+        if (moreDiv.current && !moreDiv.current.contains(e.target)) {
+           handleSetIsExpandToFalse()
+        }
+      };
+
+    
     return (
-        <div className="relative border py-5 px-3 shadow-md dark:bg-chagllenge-bg-dark dark:border-[#111519]">
+        <div  className="relative border py-5 px-3 shadow-md dark:bg-chagllenge-bg-dark dark:border-[#111519]">
             {isUpdateForm && <Mask />}
             <div className="relative flex items-center mb-5 justify-between">
                 <h2 className="challenge-title relative uppercase tracking-wider font-semibold py-3 mr-8 dark:text-dark-text-secondry">{title}</h2>
-                {isUpdateForm && <UpdateForm id={id} title={title} handleSetIsExpandToFalse={handleSetIsExpandToFalse} handleCloseUpdateForm={handleCloseUpdateForm}/>}
+                {isUpdateForm && <UpdateForm id={id} title={title} handleCloseUpdateForm={handleCloseUpdateForm}/>}
                 <div className="relative">
-                    <button onClick={handleIsExpand} className="flex justify-center items-center dark:text-white/70">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <button className="flex justify-center items-center dark:text-white/70">
+                        <svg ref={dotRef} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                         </svg>
                     </button>
-                    <div className="absolute bg-[#333945] rounded-sm text-white shadow-sm z-10 right-0 top-full translate-y-6 invisible opacity-0 py-3 flex flex-col justify-start gap-3  transition-all duration-300 aria-expanded:translate-y-2 aria-expanded:opacity-100 aria-expanded:visible" aria-expanded={`${isExpand ? "true" : "false"}`}>
+                    <div onClick={e => hideExp(e)}  ref={moreDiv} className="absolute bg-[#333945] rounded-sm text-white shadow-sm z-10 right-0 top-full translate-y-6 invisible opacity-0 py-3 flex flex-col justify-start gap-3  transition-all duration-300 aria-expanded:translate-y-2 aria-expanded:opacity-100 aria-expanded:visible" aria-expanded={`${isExpand ? "true" : "false"}`}>
                         <button onClick={() => handleOpenDeleteOneChallengeModal(index)} className="px-10 py-2 text-lef">Delete</button>
                         <button onClick={handleSetIsUpdateForm} className="px-10 py-2 text-lef">Edit</button>
                         <div className="absolute w-2 aspect-square bg-[#333945] right-2 -top-1 rotate-45"></div>
--- src/components/UpdateForm.jsx
+++ src/components/UpdateForm.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react"
 import { useAppContext } from "../AppContext"
 
 
-export default function UpdateForm({ id, title, handleSetIsExpandToFalse, handleCloseUpdateForm }) {
+export default function UpdateForm({ id, title, handleCloseUpdateForm }) {
 
     const [updateFormTitleInput, setUpdateFormTitleInput] = useState("")
 
@@ -20,7 +20,7 @@ export default function UpdateForm({ id, title, handleSetIsExpandToFalse, handle
     return (
         <div className="max-w-[500px] bg-white shadow-lg rounded-xl dark:bg-modal-bg-dark absolute p-10 top-1/2 left-1/2 -translate-x-1/2 z-20">
             <form className="flex flex-col gap-5" onSubmit={(e) => {handleUpdateChallenge(e, id, updateFormTitleInput), handleCloseUpdateForm(e)}}>
-                <input className="py-3 border border-transparent bg-gray-100 px-3 text-xl outline-none focus:border-blue-500 placeholder:text-sm placeholder:tracking-wider placeholder:uppercase dark:bg-filter-input-dark dark:text-dark-text-secondry" onFocus={handleSelect} onClick={handleSetIsExpandToFalse} ref={inputRef} value={updateFormTitleInput} onChange={(e) => setUpdateFormTitleInput(e.target.value)} type="text" />
+                <input className="py-3 border border-transparent bg-gray-100 px-3 text-xl outline-none focus:border-blue-500 placeholder:text-sm placeholder:tracking-wider placeholder:uppercase dark:bg-filter-input-dark dark:text-dark-text-secondry" onFocus={handleSelect} ref={inputRef} value={updateFormTitleInput} onChange={(e) => setUpdateFormTitleInput(e.target.value)} type="text" />
                 <div className="flex gap-3">
                     <button onClick={handleCloseUpdateForm} className="w-full capitalize tracking-wider font-bold text-sm rounded-lg bg-[#efeff9] text-[#686585] dark:bg-inherit dark:text-[#4189a3]" >close</button>
                     <button className="w-full capitalize tracking-wider font-bold text-sm rounded-lg bg-blue-700 text-white py-2 dark:bg-[#51a1be] dark:text-[#e5ebec]" >confirm</button>
